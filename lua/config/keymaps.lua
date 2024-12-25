606e7b9cbdaafbaa2ab2804c347eeb819873348a
y-not-u <voganwong@hotmail.com>
2024-12-26 00:26:31 +0800

    feat: add `ba` to delete all buffers keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,9 +5,17 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- editor
 keymap("n", "<leader>q", "<cmd>q<cr>", opts)
-keymap("i", "jk", "<esc>", opts)
+keymap("i", "jk", "<esc>", { desc = "jk to escape" })
+keymap("n", "<leader>w", "<cmd>w<cr><cmd>lua vim.notify('Saved')<cr>", opts)
+
+-- formatting
 keymap({ "n", "v" }, "<leader>F", function()
   LazyVim.format({ force = true })
 end, opts)
-keymap("n", "<leader>w", "<cmd>w<cr><cmd>lua vim.notify('Saved')<cr>", opts)
+
+-- buffers
+keymap("n", "<leader>ba", function()
+  Snacks.bufdelete.all()
+end, { desc = "Delete all buffers" })
